6a0a3aad7a99b6f03c7584faa7c69a64c1456ba0
stsong <stsong@gap.com>
2019-11-17 20:48:07 -0800

    Added add/landing home page

--- app.js
+++ app.js
@@ -13,12 +13,18 @@ db.authenticate()
 
 const app = express();
 
+// Handlebars
 app.engine('handlebars', exphbs({defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.send('INDEX');
-});
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Index route
+app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
 
 // Gig routes
 app.use('/gigs', require('./routes/gigs'));
--- routes/gigs.js
+++ routes/gigs.js
@@ -7,22 +7,21 @@ const Gig = require('../models/Gigs');
 router.get('/', (req, res) =>
     Gig.findAll()
         .then(gigs => {
-            console.log(gigs);
-            res.sendStatus(200);
+            res.render('gigs', {
+                gigs
+            });
         })
         .catch(err => console.log(err))
 );
 
-// Add a gig
-router.get('/add', (req,res) => {
-   const data =  {
-       title: "Simple wordpress website",
-       technologies: "wordpress, php, html",
-       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque finibus tempor mauris, non maximus urna lacinia nec. Morbi mollis tincidunt feugiat. Etiam ut lectus erat. Nulla euismod rutrum hendrerit. Sed sodales lorem nec massa fermentum hendrerit. Phasellus a eros eleifend, aliquam massa in, iaculis elit. Donec consequat urna a consequat aliquet. Proin a ullamcorper neque. Curabitur laoreet turpis non justo interdum consectetur. Aenean in lectus non est porta condimentum quis vitae nisi. Phasellus imperdiet tempor arcu, nec fringilla nunc lacinia a. Nulla mollis velit urna, vitae rutrum est blandit volutpat. In consectetur massa ac orci sodales tincidunt at nec arcu.",
-       contact_email: "user1@gmail.com"
-   };
+// Display add gig form
+router.get('/add', (req, res) => res.render('add'));
 
-   let { title, technologies, budget, description, contact_email } = data;
+// Add a gig
+router.post('/add', (req, res) => {
+   let { title, technologies, budget, description, contact_email } = req.body;
+
+
    // Insert into table
     Gig.create({
         title,
